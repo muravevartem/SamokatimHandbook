6abcbf8963ab84307fc7682b256f438e7b336e9c
muravevas <muravevartem@outlook.com>
2023-02-27 00:50:43 +0400

    Added ci.yml

--- src/test/java/com/muravev/samokatimhandbook/HandbookApplicationTests.java
+++ src/test/java/com/muravev/samokatimhandbook/HandbookApplicationTests.java
@@ -3,7 +3,7 @@ package com.muravev.samokatimhandbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class HandbookApplicationTests {
 
     @Test
