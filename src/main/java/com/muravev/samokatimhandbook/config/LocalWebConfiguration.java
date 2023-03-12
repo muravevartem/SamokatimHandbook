4591a9fa9dc5183a8be088b530f8bed14ce1b00c
muravevas <muravevartem@outlook.com>
2023-03-13 01:18:39 +0400

    Using optional

--- src/main/java/com/muravev/samokatimhandbook/config/LocalWebConfiguration.java
+++ src/main/java/com/muravev/samokatimhandbook/config/LocalWebConfiguration.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("local")
 @Configuration
 public class LocalWebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://1218407-cu57808.tw1.ru",
+                        "https://1218407-cu57808.tw1.ru"
+                )
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
